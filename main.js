8e893cf43e8adc4bbcafac1c71b6744f3f727479
zhongshan002 <84626081+zhongshan002@users.noreply.github.com>
2024-06-16 15:11:02 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+
+import uView from "uview-ui";
+Vue.use(uView);
+
+
+//路由守卫
+import {
+	router,
+	RouterMount
+} from './util/router.js'
+Vue.use(router)
+
+
+// 工具包
+import globalUtil from "@/util/face.js";
+Vue.prototype.globalUtil=globalUtil;
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
